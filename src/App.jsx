6eed36a4cf1a9c13a7364f1dbc31491239907055
GTklr <56957480+GTklr@users.noreply.github.com>
2023-02-18 19:02:08 +0800

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline text-green-400">
+        hello
+      </h1>
+    </div>
+  )
+}
+
+export default App
