a482d450c0f4791ba5d45c238fd70dd926116675
GTklr <56957480+GTklr@users.noreply.github.com>
2023-02-19 21:42:17 +0800

    FINALLY fixed the stupid text ediot

---
+++ src/pages/createtest.jsx
@@ -0,0 +1,16 @@
+try {
+    const file = blobInfo.blob();
+    const storageRef = ref(storage, file.name);
+    const metadata = {
+      contentType: file.type,
+    };
+    
+    await uploadBytes(storageRef, file, metadata);
+    const url = await getDownloadURL(storageRef);
+    console.log(url);
+    return url;
+  } catch (error) {
+    // Call the failure callback with the error message
+    console.log(error.message);
+  }
+};
