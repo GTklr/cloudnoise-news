976dd1e4ddc7e468f223f97f2205db29b213d5b6
GTklr <56957480+GTklr@users.noreply.github.com>
2023-02-22 20:39:12 +0800

    Loeading screen on intro

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import Hero1 from '../components/intro/Hero1'
 import TransitionsZoom from '../components/transitions/TransitionZoom'
+import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCloud } from '@fortawesome/free-solid-svg-icons'
 
 export default function Intro() {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+      
+    }, 1000);
+
+    // Clear the timeout if the component unmounts or the delay changes
+    return () => clearTimeout(timer);
+  }, []); // Only run the eff
+
   return (
-    <TransitionsZoom>
-      <Hero1 />
-    </TransitionsZoom>
+    <div>
+        {!isVisible && <div className="h-screen flex justify-center items-center">
+          <FontAwesomeIcon icon={faCloud} className="m-2 animate-bounce" size="2xl"/>
+        </div>}
+
+        {isVisible && <TransitionsZoom>
+          <Hero1 />
+        </TransitionsZoom>}
+    </div>
+    
   )
 }
-// TODO buil more components for home
-
